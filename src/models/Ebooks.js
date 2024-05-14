6b360ad8a53ff3249b009c9fc5f900ebc94b1901
Nguyễn Phúc Bảo Danh <117022027+baodanh7302@users.noreply.github.com>
2024-05-14 16:32:02 +0700

    Initial partials folder
    Partials folder in views folders, include header and footer

--- src/models/Ebooks.js
+++ src/models/Ebooks.js
@@ -3,9 +3,14 @@ const { default: mongoose } = require("mongoose");
 const { Schema } = mongoose;
 const ebookSchema = new Schema({
   title: { type: String, required: true },
+  type: { type: String, required: true},
+  language: { type: String, required: true},
   description: { type: String, required: true, unique: true },
   ebookFile: { type: String, require: true},
   imageFile: { type: String, required: true },
+  state: { type: String, require: true},
+  author: { type: String, require: true},
+  date: { type: Date, require: true}
 });
 
 const Ebook = mongoose.model("Ebook", ebookSchema);

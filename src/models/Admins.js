35f503a838cf1a93688f98cd8e9081e5b6bec535
Nguyễn Phúc Bảo Danh <117022027+baodanh7302@users.noreply.github.com>
2024-05-28 21:22:22 +0700

    initial Admin model

---
+++ src/models/Admins.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const { Schema } = mongoose;
+const adminSchema = new Schema({
+  adname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String },
+  password: { type: String, required: true },
+  date: { type: Date, require: true }
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
